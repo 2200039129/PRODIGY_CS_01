970deb3353b3b93609934f96a778e6708ba64a47
2200039129 <2200039129@kluniversity.in>
2024-06-25 19:22:02 +0530

    Add files via upload

---
+++ CS-01output.py
@@ -0,0 +1,7 @@
+C:\Users\kotaa\PycharmProjects\pythonProject11\venv\Scripts\python.exe C:\Users\kotaa\PycharmProjects\pythonProject11\main.py 
+Would you like to encrypt or decrypt the message? (e/d): e
+Enter the message: akshay
+Enter the shift value: 129
+Encrypted message: zjrgzx
+
+Process finished with exit code 0
